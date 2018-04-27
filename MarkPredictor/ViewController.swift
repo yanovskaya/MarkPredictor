6d2d0cd54287186562642c703211667242cfc805
yanovskaya <yanovskaya1997@list.ru>
2018-04-28 01:41:05 +0300

    Подготовка рабочей среды

---
+++ MarkPredictor/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MarkPredictor
+//
+//  Created by Елена Яновская on 28.04.2018.
+//  Copyright © 2018 Elena Yanovskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
